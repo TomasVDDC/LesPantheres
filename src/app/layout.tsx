c7999812cfa87e2ec5ea243b5f161f80905b921e
DiogoVDDC <diogovaldivieso@gmail.com>
2024-04-24 15:41:31 +0200

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,46 @@
+import Header from "../components/Header"
+import "./globals.css"
+import Header2 from "../components/Header2"
+import { Inter, Roboto, Poppins, Roboto_Slab } from "next/font/google"
+import { Metadata } from "next"
+import Footer from "@/components/Footer"
+
+// export const metadata: Metadata = {
+// 	title: "Leo Adams",
+// 	description: "Leo Adams' film music portfolio",
+// }
+
+export const header = Roboto_Slab({
+	weight: ["600", "700"],
+
+	subsets: ["latin"],
+})
+
+export const poppins = Poppins({
+	weight: "400",
+	subsets: ["latin"],
+	variable: "--poppins-default",
+})
+
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode
+}) {
+	return (
+		<html lang="en">
+			<body className={`${poppins.className}`}>
+				<div>
+					<div className="wave"></div>
+					<div className="wave"></div>
+					<div className="wave"></div>
+				</div>
+				<div>
+					<Header />
+					<div className="container mx-auto">{children}</div>
+				</div>
+				<Footer />
+			</body>
+		</html>
+	)
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+import { header } from "./layout"
+
+export default function Home() {
+	return (
+		<>
+			<div className="mb-[60px] md:mb-[120px]">This is the main page</div>
+		</>
+	)
+}
