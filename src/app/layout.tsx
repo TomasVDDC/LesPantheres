5175879089650609020a6f59a6874d4f17251295
DiogoVDDC <diogovaldivieso@gmail.com>
2024-04-24 16:21:49 +0200

    fixed layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,7 @@
 import Header from "../components/Header"
 import "./globals.css"
 import { Inter, Roboto, Poppins, Roboto_Slab } from "next/font/google"
-import { Metadata } from "next"
 import Footer from "@/components/Footer"
-
-// export const metadata: Metadata = {
-// 	title: "Leo Adams",
-// 	description: "Leo Adams' film music portfolio",
-// }
-
-export const header = Roboto_Slab({
-	weight: ["600", "700"],
-
-	subsets: ["latin"],
-})
 
 export const poppins = Poppins({
 	weight: "400",
