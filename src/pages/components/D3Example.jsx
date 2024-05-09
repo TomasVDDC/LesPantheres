84e1d4cccb5b756f1d9aa2ade0b61ec0e3b832eb
TomasVDDC <95030283+TomasVDDC@users.noreply.github.com>
2024-05-09 15:18:45 +0200

    changed color of skeleton

--- src/pages/components/D3Example.jsx
+++ src/pages/components/D3Example.jsx
@@ -2,10 +2,11 @@ import { useEffect, createRef } from "react";
 import "react-toastify/dist/ReactToastify.css";
 
 // Geo json files
-import countyData from "../data/counties.json";
+//import countyData from "../data/counties.json";
 import stateData from "../data/states.json";
 
 import { ToastContainer, toast } from "react-toastify";
+// example based on https://gnithyanantham.medium.com/creating-maps-using-d3-js-in-react-f42b8a292580
 
 const mapRatio = 0.5;
 
@@ -62,93 +63,21 @@ export default function D3Example({ width, height }) {
       .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom);
 
-    // Creating counties layer
     g.append("g")
-      .attr("id", "counties")
+      .attr("id", "states")
       .selectAll("path")
-      .data(countyData.features)
+      .data(stateData.features)
       .enter()
       .append("path")
-      .attr("d", pathGenerator)
       .attr("key", (feature) => {
-        return feature.properties.STATE + feature.properties.COUNTY;
+        return feature.properties.NAME;
       })
-      .attr("class", "county-boundary")
-      .attr("fill", (feature) => {
-        // I could directly call colorGenerator instead of calling it in a arrow function, I've added it in that way so that you'd know we can send values from geo json into every step of the map creation.
-        return colorGenerator();
-      });
-    //.on("click", resetZoom);
+      .attr("d", pathGenerator)
+      .attr("class", "state")
+      // Here's an example of what I was saying in my previous comment.
+      .attr("fill", colorGenerator);
+    //.on("click", handleZoom);
   }, []);
-  // // Creating state layer on top of counties layer.
-  // g.append("g")
-  //   .attr("id", "states")
-  //   .selectAll("path")
-  //   .data(stateData.features)
-  //   .enter()
-  //   .append("path")
-  //   .attr("key", (feature) => {
-  //     return feature.properties.NAME;
-  //   })
-  //   .attr("d", pathGenerator)
-  //   .attr("class", "state")
-  //   // Here's an example of what I was saying in my previous comment.
-  //   .attr("fill", colorGenerator)
-  //   .on("click", handleZoom);
-
-  // function handleZoom(stateFeature) {
-  //   // Set the state backgroud to 'none' so that the counties can be displayed.
-  //   active.classed("active", false);
-  //   active = d3.select(this).classed("active", true);
-
-  //   toast.info(`Selected state is `, {
-  //     position: "top-right",
-  //     autoClose: 5000,
-  //     hideProgressBar: true,
-  //     closeOnClick: true,
-  //     pauseOnHover: true,
-  //     draggable: true,
-  //     progress: undefined,
-  //     theme: "light",
-  //   });
-  //   // Call to zoom in.
-  //   zoomIn(stateFeature);
-  // }
-
-  // function zoomIn(currentState) {
-  //   // Get bounding box values for the selected county.
-  //   let bounds = pathGenerator.bounds(currentState);
-
-  //   // Zoom In calculations
-  //   let dx = bounds[1][0] - bounds[0][0];
-  //   let dy = bounds[1][1] - bounds[0][1];
-
-  //   let x = (bounds[0][0] + bounds[1][0]) / 2;
-  //   let y = (bounds[0][1] + bounds[1][1]) / 2;
-
-  //   let scale = 0.9 / Math.max(dx / width, dy / height);
-  //   let translate = [width / 2 - scale * x, height / 2 - scale * y];
-
-  //   // Updaing the css using D3
-  //   g.transition()
-  //     .duration(750)
-  //     .style("stroke-width", 1.5 / scale + "px")
-  //     .attr("transform", "translate(" + translate + ")scale(" + scale + ")");
-  // }
-
-  //   function resetZoom() {
-  //     // Remove the active class so that state color will be restored and conuties will be hidden again.
-  //     active.classed("active", false);
-  //     active = d3.select(null);
-
-  //     // Resetting the css using D3
-  //     g.transition()
-  //       .delay(100)
-  //       .duration(750)
-  //       .style("stroke-width", "1.5px")
-  //       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-  //   }
-  // }, []);
 
   return (
     <div>
