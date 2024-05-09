6d57465aae7248066f828793526898ada976f5fa
DiogoVDDC <diogovaldivieso@gmail.com>
2024-05-09 18:49:30 +0200

    Merge changes

--- src/pages/components/ChartSketch.tsx
+++ src/pages/components/ChartSketch.tsx
@@ -1,17 +1,23 @@
-import React from "react";
-import Image from "next/image";
+import React from "react"
+import Image from "next/image"
 
 const ChartSketch = () => {
-  return (
-    <div className="flex justify-center mt-10">
-      <Image
-        src="/sketch_charts.jpg" // no need to specify public folder next automatically searches for it
-        width={900}
-        height={900}
-        alt="sketch of the charts we will produce"
-      />
-    </div>
-  );
-};
+	return (
+		<>
+			<h1 className="text-4xl  font-bold">Charts and statistics</h1>
+			<p className="text-[20px] mt-3 italic text-zinc-300">
+				Currently a sketch of the charts we will produce
+			</p>
+			<div className="flex justify-center mt-10">
+				<Image
+					src="/sketch_charts.jpg" // no need to specify public folder next automatically searches for it
+					width={900}
+					height={900}
+					alt="sketch of the charts we will produce"
+				/>
+			</div>
+		</>
+	)
+}
 
-export default ChartSketch;
+export default ChartSketch
--- src/pages/components/D3Example.jsx
+++ src/pages/components/D3Example.jsx
@@ -1,120 +1,180 @@
-import { useEffect, createRef } from "react";
-import "react-toastify/dist/ReactToastify.css";
+import { useEffect, createRef } from "react"
+import "react-toastify/dist/ReactToastify.css"
 
 // Geo json files
-//import countyData from "../data/counties.json";
-import stateData from "../data/states.json";
 
-import airports from "../data/filtered_airports.json";
+import countyData from "../data/counties.json"
+import stateData from "../data/states.json"
+import airports from "../data/airports.json"
+import airports from "../data/filtered_airports.json"
+import { ToastContainer, toast } from "react-toastify"
 
-import { ToastContainer, toast } from "react-toastify";
-// example based on https://gnithyanantham.medium.com/creating-maps-using-d3-js-in-react-f42b8a292580
-
-const mapRatio = 0.5;
+const mapRatio = 0.5
 
 const margin = {
-  top: 10,
-  bottom: 10,
-  left: 10,
-  right: 10,
-};
-
-const colorScale = ["#B9EDDD", "#87CBB9", "#569DAA", "#577D86"];
-
-export default function D3Example({ width, height }) {
-  // A random color generator
-  const airports_data = {
-    type: "FeatureCollection",
-    features: airports.map((airport) => ({
-      type: "Feature",
-      geometry: {
-        type: "Point",
-        coordinates: [
-          parseFloat(airport.longitude),
-          parseFloat(airport.latitude),
-        ],
-      },
-    })),
-  };
-
-  const colorGenerator = () => {
-    return colorScale[Math.floor(Math.random() * 4)];
-  };
-
-  useEffect(() => {
-    let width = parseInt(d3.select(".viz").style("width"));
-
-    let height = width * mapRatio;
-    //let active = d3.select(null);
-
-    width = width - margin.left - margin.right;
-
-    const svg = d3
-      .select(".viz")
-      .append("svg")
-      .attr("class", "center-container")
-      .attr("height", height + margin.top + margin.bottom)
-      .attr("width", width + margin.left + margin.right);
-
-    svg
-      .append("rect")
-      .attr("class", "background center-container")
-      .attr("height", height + margin.top + margin.bottom)
-      .attr("width", width + margin.left + margin.right);
-
-    // Creating projection, it's best to use 'geoAlbersUsa' projection if you're rendering USA map and for other maps use 'geoMercator'.
-    const projection = d3
-      .geoAlbersUsa()
-      .translate([width / 2, height / 2])
-      .scale(width);
-
-    // Creating path generator fromt the projecttion created above.
-    const pathGenerator = d3.geoPath().projection(projection);
-
-    // Creating the container
-    const g = svg
-      .append("g")
-      .attr("class", "center-container center-items us-state")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom);
-
-    g.append("g")
-      .attr("id", "states")
-      .selectAll("path")
-      .data(stateData.features)
-      .enter()
-      .append("path")
-      .attr("key", (feature) => {
-        return feature.properties.NAME;
-      })
-      .attr("d", pathGenerator)
-      .attr("class", "state")
-      // Here's an example of what I was saying in my previous comment.
-      .attr("fill", colorGenerator);
-
-    const airport_projection = d3
-      .geoAlbersUsa()
-      .scale(width)
-      .translate([width / 2 + margin.left, height / 2 + margin.top]);
-
-    // Append airports to the map
-    const airportsGroup = svg.append("g").attr("class", "airports");
-
-    airportsGroup
-      .selectAll("path")
-      .data(airports_data.features)
-      .enter()
-      .append("path")
-      .attr("d", d3.geoPath(airport_projection).pointRadius(5))
-      .attr("fill", "red"); // Change as needed
-
-    //.on("click", handleZoom);
-  }, []);
-
-  return (
-    <div>
-      <div class="viz"></div>
-    </div>
-  );
+	top: 10,
+	bottom: 10,
+	left: 10,
+	right: 10,
 }
+
+const colorScale = ["#B9EDDD", "#87CBB9", "#569DAA", "#577D86"]
+
+export default function D3Example() {
+	// A random color generator
+	const colorGenerator = () => {
+		return colorScale[Math.floor(Math.random() * 4)]
+	}
+
+	useEffect(() => {
+		let width = parseInt(d3.select(".viz").style("width"))
+
+		let height = width * mapRatio
+		let active = d3.select(null)
+
+		width = width - margin.left - margin.right
+
+		const svg = d3
+			.select(".viz")
+			.append("svg")
+			.attr("class", "center-container")
+			.attr("height", height + margin.top + margin.bottom)
+			.attr("width", width + margin.left + margin.right)
+
+		svg
+			.append("rect")
+			.attr("class", "background center-container")
+			.attr("height", height + margin.top + margin.bottom)
+			.attr("width", width + margin.left + margin.right)
+
+		// Creating projection, it's best to use 'geoAlbersUsa' projection if you're rendering USA map and for other maps use 'geoMercator'.
+		const projection = d3
+			.geoAlbersUsa()
+			.translate([width / 2, height / 2])
+			.scale(width)
+
+		// Creating path generator fromt the projecttion created above.
+		const pathGenerator = d3.geoPath().projection(projection)
+
+		// Creating the container
+		const g = svg
+			.append("g")
+			.attr("class", "center-container center-items us-state")
+			.attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+			.attr("width", width + margin.left + margin.right)
+			.attr("height", height + margin.top + margin.bottom)
+
+		g.append("g")
+			.attr("id", "states")
+			.selectAll("path")
+			.data(stateData.features)
+			.enter()
+			.append("path")
+			.attr("key", (feature) => {
+				return feature.properties.NAME
+			})
+			.attr("d", pathGenerator)
+			.attr("fill", "#ffffff")
+
+		svg
+			.selectAll("circle")
+			.data(airports)
+			.enter()
+			.append("circle")
+			.attr(
+				"transform",
+				(d) => `translate(${projection([d.LONGITUDE, d.LATITUDE])})`
+			)
+			.attr("r", 5) // Adjust the radius of the dots as needed
+			.attr("fill", "red") // Adjust the color of the dots as needed
+	}, [])
+
+	return (
+		<div>
+			<h1 className="text-4xl  font-bold">Main visualization</h1>
+			<p className="text-[20px] italic mt-3 text-zinc-300">
+				Currently a simple D3 skeleton
+			</p>
+			<div class="viz"></div>
+		</div>
+	)
+}
+
+// // Creating state layer on top of counties layer.
+
+// function handleZoom(stateFeature) {
+//   // Set the state backgroud to 'none' so that the counties can be displayed.
+//   active.classed("active", false);
+//   active = d3.select(this).classed("active", true);
+
+//   toast.info(`Selected state is `, {
+//     position: "top-right",
+//     autoClose: 5000,
+//     hideProgressBar: true,
+//     closeOnClick: true,
+//     pauseOnHover: true,
+//     draggable: true,
+//     progress: undefined,
+//     theme: "light",
+//   });
+//   // Call to zoom in.
+//   zoomIn(stateFeature);
+// }
+
+// function zoomIn(currentState) {
+//   // Get bounding box values for the selected county.
+//   let bounds = pathGenerator.bounds(currentState);
+
+//   // Zoom In calculations
+//   let dx = bounds[1][0] - bounds[0][0];
+//   let dy = bounds[1][1] - bounds[0][1];
+
+//   let x = (bounds[0][0] + bounds[1][0]) / 2;
+//   let y = (bounds[0][1] + bounds[1][1]) / 2;
+
+//   let scale = 0.9 / Math.max(dx / width, dy / height);
+//   let translate = [width / 2 - scale * x, height / 2 - scale * y];
+
+//   // Updaing the css using D3
+//   g.transition()
+//     .duration(750)
+//     .style("stroke-width", 1.5 / scale + "px")
+//     .attr("transform", "translate(" + translate + ")scale(" + scale + ")");
+// }
+
+//   function resetZoom() {
+//     // Remove the active class so that state color will be restored and conuties will be hidden again.
+//     active.classed("active", false);
+//     active = d3.select(null);
+
+//     // Resetting the css using D3
+//     g.transition()
+//       .delay(100)
+//       .duration(750)
+//       .style("stroke-width", "1.5px")
+//       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+//   }
+// }, []);
+
+// .attr("class", "state")
+// Here's an example of what I was saying in my previous comment.
+// .attr("fill", colorGenerator)
+// .on("click", handleZoom)
+
+// Creating counties layer
+// g.append("g")
+// 	.attr("id", "counties")
+// 	.selectAll("path")
+// 	.data(countyData.features)
+// 	.enter()
+// 	.append("path")
+// 	.attr("d", pathGenerator)
+// 	.attr("key", (feature) => {
+// 		return feature.properties.STATE + feature.properties.COUNTY
+// 	})
+// 	.attr("class", "county-boundary")
+// 	.attr("fill", (feature) => {
+// 		// I could directly call colorGenerator instead of calling it in a arrow function, I've added it in that way so that you'd know we can send values from geo json into every step of the map creation.
+// 		return colorGenerator()
+// 	})
+//.on("click", resetZoom);
