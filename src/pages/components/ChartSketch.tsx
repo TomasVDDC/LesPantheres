1cc367f8cfdfb2bd68883fe01045467579e7e992
TomasVDDC <95030283+TomasVDDC@users.noreply.github.com>
2024-05-10 11:58:18 +0200

    Revert "Merge changes"
    This reverts commit 6d57465aae7248066f828793526898ada976f5fa.

--- src/pages/components/ChartSketch.tsx
+++ src/pages/components/ChartSketch.tsx
@@ -1,23 +1,17 @@
-import React from "react"
-import Image from "next/image"
+import React from "react";
+import Image from "next/image";
 
 const ChartSketch = () => {
-	return (
-		<>
-			<h1 className="text-4xl  font-bold">Charts and statistics</h1>
-			<p className="text-[20px] mt-3 italic text-zinc-300">
-				Currently a sketch of the charts we will produce
-			</p>
-			<div className="flex justify-center mt-10">
-				<Image
-					src="/sketch_charts.jpg" // no need to specify public folder next automatically searches for it
-					width={900}
-					height={900}
-					alt="sketch of the charts we will produce"
-				/>
-			</div>
-		</>
-	)
-}
+  return (
+    <div className="flex justify-center mt-10">
+      <Image
+        src="/sketch_charts.jpg" // no need to specify public folder next automatically searches for it
+        width={900}
+        height={900}
+        alt="sketch of the charts we will produce"
+      />
+    </div>
+  );
+};
 
-export default ChartSketch
+export default ChartSketch;
--- src/pages/components/Header.tsx
+++ src/pages/components/Header.tsx
@@ -1,30 +1,30 @@
-import React from "react"
-import Link from "next/link"
+import React from "react";
+import Link from "next/link";
 //import { Zoom } from "react-awesome-reveal";
 
 const Header = () => {
-	const navLinks = [
-		// { name: "About", path: "/about" },
-		// { name: "Work", path: "/" },
-		// { name: "Sheet music", path: "/sheet-music" },
-		// { name: "Contact", path: "/contact" },
-	]
+  const navLinks = [
+    // { name: "About", path: "/about" },
+    // { name: "Work", path: "/" },
+    // { name: "Sheet music", path: "/sheet-music" },
+    // { name: "Contact", path: "/contact" },
+  ];
 
-	return (
-		<div className="mx-auto mb-[50px]">
-			{/* <Zoom duration={500} triggerOnce> */}
-			<header
-				className={`flex flex-col sm:text-center  md:text-left md:flex-row justify-between items-center  mx-auto py-4  mt-[30px]  md:mt-[80px]`}
-			>
-				<div>
-					<Link className={`text-[60px]  font-bold text-white`} href="/">
-						Plane visualization
-					</Link>
-					<p className="text-[20px] text-zinc-300">
-						Data Visualization EPFL 2024
-					</p>
-				</div>
-				{/* <div className="flex space-x-10 mt-5 md:mt-0">
+  return (
+    <div className="container mx-auto mb-[50px]">
+      {/* <Zoom duration={500} triggerOnce> */}
+      <header
+        className={`flex flex-col sm:text-center  md:text-left md:flex-row justify-between items-center  mx-auto py-4  mt-[30px]  md:mt-[80px]`}
+      >
+        <div>
+          <p className={`text-[60px] font-bold text-white`} >
+            Plane visualization
+          </p>
+          <p className="text-[20px] text-zinc-300">
+            Data Visualization EPFL 2024
+          </p>
+        </div>
+        {/* <div className="flex space-x-10 mt-5 md:mt-0">
 						{navLinks.map((link, index) => {
 							return (
 								<NavLink
@@ -36,11 +36,11 @@ const Header = () => {
 							)
 						})}
 					</div> */}
-			</header>
-			<hr />
-			{/* </Zoom> */}
-		</div>
-	)
-}
+      </header>
+      <hr />
+      {/* </Zoom> */}
+    </div>
+  );
+};
 
-export default Header
+export default Header;
