b4ae364aa5dd816baa75c8a6fee17707c7ead2e0
TomasVDDC <95030283+TomasVDDC@users.noreply.github.com>
2024-05-31 09:05:14 +0200

    removed general statistics title

--- src/pages/components/GeneralStatistics.jsx
+++ src/pages/components/GeneralStatistics.jsx
@@ -1,53 +1,3 @@
-// import React, { useEffect } from "react";
-// import Chart from "chart.js/auto";
-// import departure_delays from "@/data/departure_delays.json";
-// import diverted_flights from "@/data/diverted_flights.json";
-
-// import { Colors } from "chart.js";
-
-// const GeneralStatistics = () => {
-//   useEffect(() => {
-//     Chart.defaults.color = "#FFFFFF"; // Set default text color to white
-//     Chart.register(Colors);
-
-//     const data = {
-//       labels: diverted_flights.labels,
-//       datasets: [
-//         {
-//           label: "Number of diverted flights",
-//           data: diverted_flights.data,
-//           backgroundColor: "rgba(255, 99, 132, 0.8)",
-//         },
-//       ],
-//     };
-
-//     const config = {
-//       type: "bar",
-//       data: data,
-//       options: {
-//         options: {
-//           scales: {
-//             y: {
-//               beginAtZero: true,
-//             },
-//           },
-//         },
-//       },
-//     };
-
-//     new Chart(document.getElementById("dimensions"), config);
-//   }, []);
-
-//   return (
-//     <div>
-//       <div className="w-[500px]">
-//         <canvas id="dimensions"></canvas>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default GeneralStatistics;
 import React, { useEffect, useRef, useState } from "react";
 import Chart from "chart.js/auto";
 import departure_delays from "@/data/departure_delays.json";
@@ -135,7 +85,7 @@ const GeneralStatistics = () => {
 
   return (
     <div>
-      <h1 className="text-3xl font-semibold ">{chartTitle}</h1>
+      {/* <h1 className="text-3xl font-semibold ">{chartTitle}</h1> */}
       <div className="w-[500px] flex justify-center" onClick={updateChart}>
         <canvas id="dimensions" className="canvas-clickable"></canvas>
       </div>
